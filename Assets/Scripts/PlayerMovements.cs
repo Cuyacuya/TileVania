1a7717634173bf0a194af40510baf79fd0d65690
Cuyacuya <59507274+Cuyacuya@users.noreply.github.com>
2024-09-01 09:40:25 +0900

    🚀 Feat : #3 Jump 기능, Cinemachine, 점프 오브젝트 생성 및 벽 점프 방지
    - Jump 기능 생성
      - 땅에 닿았을 때만 점프 가능
      - 점프 애니메이션 적용
    
    - Cinemachine 생성
      - Confiner 설정
      - 상태 중심 카메라 설정
    
    - 점프 오브젝트 생성
    
    - 벽 점프 막기

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -6,27 +6,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 5f;
-    Vector2 moveInput;
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
+    Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    CapsuleCollider2D myBodyCollider;
     Animator myAnimator;
-    
+    BoxCollider2D myFeetCollider;
+
+    float gravityScaleAtStart;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbingLadder();
     }
-
+     
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
 
     void Run()
@@ -46,5 +65,22 @@ public class PlayerMovements : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f); //Sign : 0 또는 양수일 때 1을 반환, 음수일 때 -1을 반환
         }    
+    }
+
+    void ClimbingLadder()
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        { 
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("isClimbing", false);
+            return; 
+        }
+            
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        myRigidbody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }
